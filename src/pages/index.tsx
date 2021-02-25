498b295d4b9cfecb0b10747e84a8343919b2966c
Nishino0719 <nishino0719@icloud.com>
2021-02-25 11:24:03 +0900

    :tada: first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <div className="">
+      <h1 className="text-3xl ">hello world</h1>
+    </div>
+    </>
+  )
+}
